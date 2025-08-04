fcfd67f4808fce328b46f4ae3994b6fa98e5a1cc
AlekseiEvgenievich <63292078+AlekseiEvgenievich@users.noreply.github.com>
2025-08-04 17:02:38 +0200

    Update vector.cc

--- vector.cc
+++ vector.cc
@@ -16,7 +16,7 @@ public:
         }
         else{
             size = size_;
-            capacity = 1.5*capacity;
+            capacity = 1.5*size;
             array = new int[capacity];
             for(int i=0;i<size;++i){
                 array[i] = array_[i];
@@ -35,7 +35,7 @@ public:
         }
         else{
             std::cout << "bbb" <<size<< std::endl;
-            capacity = 1.5*capacity;
+            capacity = 2*size;
             int* result = new int[capacity];
             for(int i=0;i<size;++i){
                 result[i] = array[i];
@@ -45,6 +45,79 @@ public:
             delete [] array;
             array = result;
         }
+    }
+    void pop_back(int a){
+        std::cout << "jjjj" << std::endl;
+        if ((size+1) < capacity){
+            std::cout << "jjjj" <<size<< std::endl;
+            int* result = new int[size+1];
+            result[0] = a;
+            for(int i=1;i<=size;++i){
+                result[i] = array[i-1];
+            }
+            size = size + 1;
+            delete [] array;
+            array = result;
+        }
+        else{
+            std::cout << "bbb" <<size<< std::endl;
+            capacity = 2*size;
+            int* result = new int[capacity];
+            result[0] = a;
+            for(int i=1;i<=size;++i){
+                result[i] = array[i-1];
+            }
+            size += 1;
+            delete [] array;
+            array = result;
+        }
+    }
+    void insert(int a,int index){
+        std::cout << "jjjj" << std::endl;
+        if ((size+1) < capacity){
+            std::cout << "jjjj" <<size<< std::endl;
+            int* result = new int[size+1];
+            //result[0] = a;
+            for(int i = 0;i<index;++i){
+                result[i] = array[i];
+            }
+            result[index] = a;
+            for(int i = index+1;i<=size;++i){
+                result[i] = array[i-1];
+            }
+            size = size + 1;
+            delete [] array;
+            array = result;
+        }
+        else{
+            std::cout << "bbb" <<size<< std::endl;
+            capacity = 2*size;
+            int* result = new int[capacity];
+            for(int i = 0;i<index;++i){
+                result[i] = array[i];
+            }
+            result[index] = a;
+            for(int i = index+1;i<=size;++i){
+                result[i] = array[i-1];
+            }
+            size = size + 1;
+            delete [] array;
+            array = result;
+        }
+    }
+    void remove(int index){
+            std::cout << "jjjj" <<size<< std::endl;
+            int* result = new int[size-1];
+            for(int i = 0;i<index;++i){
+                result[i] = array[i];
+            }
+            for(int i = index;i<size;++i){
+                result[i] = array[i+1];
+            }
+            size = size - 1;
+            delete [] array;
+            array = result;
+        
     }
     void Print(){
         for(int i=0;i<size;++i){
@@ -56,8 +129,10 @@ public:
 int main(){
     int a[3] = {2,3,5};
     Vector v(a,3);
-    v.Print();
-    v.push_back(7);
+  //  v.Print();
+  //  v.pop_back(7);
+  //  v.Print();
+    v.remove(2);
     v.Print();
     return 1;
 }
